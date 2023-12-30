637e42ca41385b2253c8dd72b814373e504c0464
shahdgethub <155221099+shahdgethub@users.noreply.github.com>
2023-12-30 10:10:17 +0200

    shahd

---
+++ calc.py
@@ -0,0 +1,17 @@
+
+
+
+def add(a, b):
+	return (a+b)
+
+def subtract(a, b):
+	return (a-b)
+
+
+def spam(mytxt):
+    print(mytxt, 'sharshar')
+    
+    
+print(add(1,2))
+print(subtract(4,2))
+print(spam("hello"))
